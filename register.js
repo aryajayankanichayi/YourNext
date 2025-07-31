57cbefa810bc00c15ae0b743afca954c93172e81
aryajayankanichayi <130576741+aryajayankanichayi@users.noreply.github.com>
2025-07-31 18:41:30 +0530

    Add files via upload
    updated with js file

---
+++ register.js
@@ -0,0 +1,9 @@
+document.querySelector('.submit').onclick = function() {
+    const pwd = document.querySelector('#pwd').value;
+    const cpwd = document.querySelector('#cpwd').value;
+    if(pwd !== cpwd) {
+    alert("Incorrect password");
+    } else {
+    alert("Submitted succesfully");
+    }
+};
